1c7a8c6ccc33406073c7a876410582d3e5c990a9
jsongo <jsongo@qq.com>
2018-05-19 01:52:28 +0800

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTabsModule } from '@angular/material/tabs';
-import { MainNavComponent } from './main-nav/main-nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 import { MyTableComponent } from './my-table/my-table.component';
-import { MyCompComponent } from './my-comp/my-comp.component';
 import { MyNavComponent } from './my-nav/my-nav.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MainNavComponent,
         MyTableComponent,
-        MyCompComponent,
-        MyNavComponent
+        MyNavComponent,
     ],
     imports: [
         BrowserModule,
